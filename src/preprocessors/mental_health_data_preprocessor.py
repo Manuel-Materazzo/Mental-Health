926632dfae3d38ab646bf60e1e91c0e242a5a069
Manuel <manuelmaterazzo@gmail.com>
2024-12-21 17:31:59 +0100

    staticize data preprocessor

--- src/preprocessors/mental_health_data_preprocessor.py
+++ src/preprocessors/mental_health_data_preprocessor.py
@@ -78,102 +78,110 @@ cols_profession_to_delete = [
 ]
 
 
-class MentalHealthDataPreprocessor(DataPreprocessor):
+def handle_null(X):
+    # delete weird professions
+    X['Profession'] = X['Profession'].mask(X['Profession'].isin(cols_profession_to_delete))
 
-    def handle_null(self, X):
-        # delete weird professions
-        X['Profession'] = X['Profession'].mask(X['Profession'].isin(cols_profession_to_delete))
+    # create "student" profession (disabled, confuses the model)
+    # X.loc[(X['Working_Professional_or_Student'] == 'Student') & (X['Profession'].isnull()), 'Profession'] = 'Student'
 
-        # create "student" profession (disabled, confuses the model)
-        # X.loc[(X['Working_Professional_or_Student'] == 'Student') & (X['Profession'].isnull()), 'Profession'] = 'Student'
+    # merge satisfaction and pressure columns and 0fill remainings (disabled, confuses the model)
+    # X['Satisfaction'] = X['Study_Satisfaction'].fillna(X['Job_Satisfaction'])
+    # X['Pressure'] = X['Academic_Pressure'].fillna(X['Work_Pressure'])
+    # X['Satisfaction'] = X['Satisfaction'].fillna(0)
+    # X['Pressure'] = X['Pressure'].fillna(0)
+    # X.drop(['Study_Satisfaction'], axis=1, inplace=True)
+    # X.drop(['Job_Satisfaction'], axis=1, inplace=True)
+    # X.drop(['Academic_Pressure'], axis=1, inplace=True)
+    # X.drop(['Work_Pressure'], axis=1, inplace=True)
 
-        # merge satisfaction and pressure columns and 0fill remainings (disabled, confuses the model)
-        # X['Satisfaction'] = X['Study_Satisfaction'].fillna(X['Job_Satisfaction'])
-        # X['Pressure'] = X['Academic_Pressure'].fillna(X['Work_Pressure'])
-        # X['Satisfaction'] = X['Satisfaction'].fillna(0)
-        # X['Pressure'] = X['Pressure'].fillna(0)
-        # X.drop(['Study_Satisfaction'], axis=1, inplace=True)
-        # X.drop(['Job_Satisfaction'], axis=1, inplace=True)
-        # X.drop(['Academic_Pressure'], axis=1, inplace=True)
-        # X.drop(['Work_Pressure'], axis=1, inplace=True)
+    # fill metric numericals with values that make sense (eg: if working, then no study stress)
+    X['Academic_Pressure'] = X['Academic_Pressure'].fillna(0)
+    X['Work_Pressure'] = X['Work_Pressure'].fillna(0)
+    X['Study_Satisfaction'] = X['Study_Satisfaction'].fillna(0)
+    X['Job_Satisfaction'] = X['Job_Satisfaction'].fillna(0)
+    X['CGPA'] = X['CGPA'].fillna(-1)  # we want to emphasize that it's a missing value
+    X['Financial_Stress'] = X['Financial_Stress'].fillna(-1)  # we want to emphasize that it's a missing value
 
-        # fill metric numericals with values that make sense (eg: if working, then no study stress)
-        X['Academic_Pressure'] = X['Academic_Pressure'].fillna(0)
-        X['Work_Pressure'] = X['Work_Pressure'].fillna(0)
-        X['Study_Satisfaction'] = X['Study_Satisfaction'].fillna(0)
-        X['Job_Satisfaction'] = X['Job_Satisfaction'].fillna(0)
-        X['CGPA'] = X['CGPA'].fillna(-1)  # we want to emphasize that it's a missing value
-        X['Financial_Stress'] = X['Financial_Stress'].fillna(-1)  # we want to emphasize that it's a missing value
+    # fill unknown categoricals
+    X['Profession'] = X['Profession'].fillna('Unknown')
+    X['Dietary_Habits'] = X['Dietary_Habits'].fillna('Unknown')
+    X['Degree'] = X['Degree'].fillna('Unknown')
 
-        # fill unknown categoricals
-        X['Profession'] = X['Profession'].fillna('Unknown')
-        X['Dietary_Habits'] = X['Dietary_Habits'].fillna('Unknown')
-        X['Degree'] = X['Degree'].fillna('Unknown')
+    # Assign values based on the dictionaries and set -1 for empty values
+    X['Sleep_Duration'] = X['Sleep_Duration'].apply(lambda x: sleep_dictionary.get(x, -1) if x != '' else -1)
+    X['Dietary_Habits'] = X['Dietary_Habits'].apply(lambda x: diet_dictionary.get(x, -1) if x != '' else -1)
+    X['Gender'] = X['Gender'].apply(lambda x: gender_dictionary.get(x, -1) if x != '' else -1)
+    X['Have_you_ever_had_suicidal_thoughts_'] = X['Have_you_ever_had_suicidal_thoughts_'].apply(
+        lambda x: yes_no_dictionary.get(x, -1) if x != '' else -1)
+    X['Working_Professional_or_Student'] = X['Working_Professional_or_Student'].apply(
+        lambda x: student_profession_dictionary.get(x, -1) if x != '' else -1)
+    X['Family_History_of_Mental_Illness'] = X['Family_History_of_Mental_Illness'].apply(
+        lambda x: yes_no_dictionary.get(x, -1) if x != '' else -1)
 
-        # Assign values based on the dictionaries and set -1 for empty values
-        X['Sleep_Duration'] = X['Sleep_Duration'].apply(lambda x: sleep_dictionary.get(x, -1) if x != '' else -1)
-        X['Dietary_Habits'] = X['Dietary_Habits'].apply(lambda x: diet_dictionary.get(x, -1) if x != '' else -1)
-        X['Gender'] = X['Gender'].apply(lambda x: gender_dictionary.get(x, -1) if x != '' else -1)
-        X['Have_you_ever_had_suicidal_thoughts_'] = X['Have_you_ever_had_suicidal_thoughts_'].apply(
-            lambda x: yes_no_dictionary.get(x, -1) if x != '' else -1)
-        X['Working_Professional_or_Student'] = X['Working_Professional_or_Student'].apply(
-            lambda x: student_profession_dictionary.get(x, -1) if x != '' else -1)
-        X['Family_History_of_Mental_Illness'] = X['Family_History_of_Mental_Illness'].apply(
-            lambda x: yes_no_dictionary.get(x, -1) if x != '' else -1)
 
-    def compute_hours_column(self, row, professional):
-        if row['Working_Professional_or_Student'] == professional:
-            return row['Work_Study_Hours']
-        else:
-            return 0
+def compute_hours_column(row, professional):
+    if row['Working_Professional_or_Student'] == professional:
+        return row['Work_Study_Hours']
+    else:
+        return 0
 
-    def compute_work_stress_column(self, row):
-        if row['Working_Professional_or_Student'] == 1:
-            return row['Financial_Stress'] + row['Work_Pressure'] - row['Job_Satisfaction']
-        else:
-            return 0
 
-    def compute_academic_stress_column(self, row):
-        if row['Working_Professional_or_Student'] == 0:
-            return row['Financial_Stress'] + row['Academic_Pressure'] - row['Study_Satisfaction']
-        else:
-            return 0
+def compute_work_stress_column(row):
+    if row['Working_Professional_or_Student'] == 1:
+        return row['Financial_Stress'] + row['Work_Pressure'] - row['Job_Satisfaction']
+    else:
+        return 0
 
-    def create_features(self, X):
-        # create additional stress columns
-        X['Work_Stress'] = X.apply(lambda row: self.compute_work_stress_column(row), axis=1)
-        X['Academic_Stress'] = X.apply(lambda row: self.compute_academic_stress_column(row), axis=1)
 
-        # distinct study and work hours
-        X['Work_Hours'] = X.apply(lambda row: self.compute_hours_column(row, 1), axis=1)
-        X['Study_Hours'] = X.apply(lambda row: self.compute_hours_column(row, 0), axis=1)
+def compute_academic_stress_column(row):
+    if row['Working_Professional_or_Student'] == 0:
+        return row['Financial_Stress'] + row['Academic_Pressure'] - row['Study_Satisfaction']
+    else:
+        return 0
 
-        X.drop(['Work_Study_Hours'], axis=1, inplace=True)
 
-    def drop_features(self, X):
-        X.drop(['Name'], axis=1, inplace=True)
-        X.drop(['City'], axis=1, inplace=True)
+def create_features(X):
+    # create additional stress columns
+    X['Work_Stress'] = X.apply(lambda row: compute_work_stress_column(row), axis=1)
+    X['Academic_Stress'] = X.apply(lambda row: compute_academic_stress_column(row), axis=1)
 
-    def fix_name(self, degree):
-        # if it's not a string, do nothing
-        if not isinstance(degree, str):
-            return degree
+    # distinct study and work hours
+    X['Work_Hours'] = X.apply(lambda row: compute_hours_column(row, 1), axis=1)
+    X['Study_Hours'] = X.apply(lambda row: compute_hours_column(row, 0), axis=1)
 
-        # remove special characters
-        degree = re.sub('[^A-Za-z0-9_]+', '', degree)
+    X.drop(['Work_Study_Hours'], axis=1, inplace=True)
 
-        # lowercase
-        degree = degree.lower()
 
+def drop_features(X):
+    X.drop(['Name'], axis=1, inplace=True)
+    X.drop(['City'], axis=1, inplace=True)
+
+
+def fix_name(degree):
+    # if it's not a string, do nothing
+    if not isinstance(degree, str):
         return degree
 
-    def fix_categoricals(self, X):
-        # standardize names to avoid duplicates and misspellings
-        X['Degree'] = X['Degree'].apply(self.fix_name)
-        X['Profession'] = X['Profession'].apply(self.fix_name)
+    # remove special characters
+    degree = re.sub('[^A-Za-z0-9_]+', '', degree)
+
+    # lowercase
+    degree = degree.lower()
+
+    return degree
+
+
+def fix_categoricals(X):
+    # standardize names to avoid duplicates and misspellings
+    X['Degree'] = X['Degree'].apply(fix_name)
+    X['Profession'] = X['Profession'].apply(fix_name)
+
+
+class MentalHealthDataPreprocessor(DataPreprocessor):
 
     def preprocess_data(self, X: DataFrame):
-        self.handle_null(X)
+        handle_null(X)
         # self.create_features(X)  # Disabled, does not improve CV
-        self.drop_features(X)
-        self.fix_categoricals(X)
+        drop_features(X)
+        fix_categoricals(X)
